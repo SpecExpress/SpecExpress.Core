daab823080d8dbaac39ae716828f3d275462ac52
Randy Bell <rbell@irunon.net>
2017-08-04 05:39:45 -0600

    Started Container

---
+++ SpecExpress.Core/SpecificationContainer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpecExpress.Core
+{
+    internal class SpecificationContainer
+    {
+        static SpecificationContainer _instance;
+
+        IServiceProvider serviceProvider;
+
+        private SpecificationContainer() { }
+
+        public 
+
+        public static SpecificationContainer Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    throw new ApplicationException("Must Builde Specification Container first!");
+                }
+                return _instance;
+            }
+        }
+
+        
+    }
+}
